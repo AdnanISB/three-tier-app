6080e21d33d67fcb103fe80fe1ee48a53b41cf54
Ubuntu <ubuntu@ip-172-31-29-54.ec2.internal>
2026-01-07 18:33:14 +0000

    files added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 function App() {
   const [items, setItems] = useState([]);
   const [name, setName] = useState('');
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://54.159.26.46:5000';
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://3.90.186.230:5000';
 
   const fetchItems = useCallback(async () => {
     const res = await axios.get(`${API_BASE_URL}/api/items`);
